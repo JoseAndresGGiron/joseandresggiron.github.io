52dc8d16b1f791c6fef324cb65f139fddcecb9f7
JoseAndresGGiron <titogg@byui.edu>
2021-01-07 21:44:52 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+let daynames=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+
+document.getElementById("currentdate").textContent = document.lastModified;
